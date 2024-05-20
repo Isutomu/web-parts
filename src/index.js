62b4a67dc4c56b31673371e9ca067306380e448f
Isutomu <isutomu@gmail.com>
2024-05-20 16:35:05 -0300

    Make the module for the sidebar

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 import './style/reset.css';
 import './style/common.css';
+import './style/sidebar.css';
+
+import { showSidebar, hideSidebar } from './modules/dropdown-menu.js';
+
+const openSidebar = document.querySelector('#open-sidebar');
+const closeSidebar = document.querySelector('#close-sidebar');
+const sidebar = document.querySelector('#sidebar');
+
+openSidebar.onclick = () => showSidebar(sidebar);
+closeSidebar.onclick = () => hideSidebar(sidebar);
+
+hideSidebar(sidebar);
---
+++ src/modules/dropdown-menu.js
@@ -0,0 +1,7 @@
+export function showSidebar(sidebar) {
+  sidebar.style.visibility = 'visible';
+}
+
+export function hideSidebar(sidebar) {
+  sidebar.style.visibility = 'hidden';
+}
